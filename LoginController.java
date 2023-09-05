3f31b38b8df6522c3b10eb4b041a6f645f4bc4de
Shreya Kushwaha <131970218+shreyakushw@users.noreply.github.com>
2023-09-05 17:22:16 +0530

    commit
    Controller for the login page.

---
+++ LoginController.java
@@ -0,0 +1,132 @@
+package com.cdac.kms;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class LoginController {
+	
+	@FXML
+	private Button loginButton;
+	@FXML
+	private Hyperlink signupclick;
+	@FXML
+	Stage stage;
+	@FXML
+	AnchorPane root;
+	
+	@FXML
+	private TextField usernameEntry;
+	@FXML
+	private PasswordField password_lEntry;
+	
+	private static void showAlert(Alert.AlertType alertType, Stage stage, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(stage);
+        alert.show();
+    }
+	String errorStyle = String.format("-fx-border-color: RED; -fx-border-width: 2; -fx-border-radius: 2;");
+	String successStyle = String.format("-fx-border-color: #A9A9A9; -fx-border-width: 2; -fx-border-radius: 2;");
+	
+	
+	
+	@FXML
+	public void clickButton(ActionEvent mousEvent) throws IOException {
+		
+		
+		if(mousEvent.getSource()==loginButton) {
+			
+			if(usernameEntry.getText().isEmpty()) {
+				usernameEntry.setStyle(errorStyle);
+				showAlert(Alert.AlertType.ERROR, stage, "Form Error!", "Login field is empty");
+				return;
+			}else {
+				usernameEntry.setStyle(successStyle);
+			}
+			if(password_lEntry.getText().isEmpty()) {
+				password_lEntry.setStyle(errorStyle);
+				showAlert(Alert.AlertType.ERROR, stage, "Form Error!", "Login field is empty");
+				return;
+			}else {
+				password_lEntry.setStyle(successStyle);
+			}
+			
+			String username = usernameEntry.getText();
+			String password = password_lEntry.getText();
+			
+			Connection conn = null;
+			ResultSet rs = null;
+			PreparedStatement ps = null;
+			
+			String query = "select * from Login_Info where Username = ? and password_l = ?";
+			
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/VS_Organizer", "root", "SdF#6N119");
+				
+				ps = conn.prepareStatement(query);
+	            ps.setString(1, username);
+	            ps.setString(2, password);
+	            rs = ps.executeQuery();
+	            if (!rs.next()) {
+	            	showAlert(Alert.AlertType.ERROR, stage, "Login Error!", "Incorrect login info");
+					return;
+	            }else {
+	       //     	String username2 = usernameEntry.getText().toString(); 
+	      //      	FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/PortfolioPage.fxml"));
+	            	
+	            	
+	            	stage = (Stage) loginButton.getScene().getWindow();
+	   			 	root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/HomePage.fxml"));
+	   			 	FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/HomePage.fxml"));
+	   		//	 	HomeController portfolio = loader.<HomeController>getController();
+	   			 	HomeController portfolio = new HomeController();
+	            	portfolio.getDetails(usernameEntry.getText());
+	            	loader.setController(portfolio);
+	   			 	
+	   		//	Stage stage = new Stage();
+	   			 	stage.setScene(new Scene(root));
+	   			 	stage.setTitle("Virtual Space Organiser");
+	   			 	stage.show();
+	            }
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					if (ps != null) {
+						ps.close();
+					}
+					if (conn != null) {
+						conn.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}	
+		}
+		if(mousEvent.getSource()==signupclick) {
+			stage = (Stage) signupclick.getScene().getWindow();
+			 root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/RegistrationPage.fxml"));
+			
+			stage.setScene(new Scene(root));
+			stage.show();
+		}	
+	}
+}
